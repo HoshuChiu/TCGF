5f8ce55507dead475740b0f39a1a36b41be6ef32
HoshuChiu <129569557+HoshuChiu@users.noreply.github.com>
2024-07-28 01:16:42 +0800

    引入SQLite

--- CardPack/CardLoader.gd
+++ CardPack/CardLoader.gd
@@ -1,11 +1,29 @@
 extends Object
 class_name CardLoader
 
-class CardResource extends Object:
-	var type:BasicCard.CardType
-	var img_path:String
-	var script_path:String
+static var _initialized = false
+const verbosity_level : int = SQLite.VERBOSE
+var db_name := "res://CardPack/demo.db"
+static var CardPackDB:Dictionary
 
-static func load(pack:String,id:int)->CardResource:
-	return CardResource.new()
+static func init():
+	var dir=DirAccess.open("res://CardPack")
+	if dir:
+		var dirs=dir.get_directories()
+		
+		for packdir:String in dirs:
+			var db=SQLite.new()
+			db.path="res://CardPack/"+packdir+"/index.db"
+			db.verbosity_level=SQLite.VERBOSE
+			db.open_db()
+
+	pass
+
+static func load(pack:String,id:int)->BasicCard:
 	
+	return BasicCard.new()
+	
+static func clean():
+	for packdb in CardPackDB:
+		#packdb.close_db()
+		pass
